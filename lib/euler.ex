31cf49f3d0149780a1abbe083ed473ef5427de62
Adam Zaninovich <adam.zaninovich@gmail.com>
2016-09-28 23:42:48 -0500

    adds problem 1

--- lib/euler.ex
+++ lib/euler.ex
@@ -1,2 +1,7 @@
 defmodule Euler do
+  def problem_1(limit) do
+    (1..limit-1)
+    |> Stream.filter(fn(num) -> 0 == rem(num, 3) or 0 == rem(num, 5) end)
+    |> Enum.sum()
+  end
 end
--- test/euler_test.exs
+++ test/euler_test.exs
@@ -1,3 +1,8 @@
 defmodule EulerTest do
   use ExUnit.Case, async: true
+
+  test "problem 1 gets the right answer" do
+    assert Euler.problem_1(10) == 23
+    assert Euler.problem_1(1000) == 233168
+  end
 end
